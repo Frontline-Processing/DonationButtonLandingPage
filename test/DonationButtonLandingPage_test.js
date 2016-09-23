b53c8fa1bc5ea79cfbb38db2b07de9534b06ac59
retsu8 <teck.geek@hotmail.com>
2016-09-23 14:54:02 -0600

    Changing to grunt and adding all the files plus linking the css and js with grunt and uglifying and minifying everything.

---
+++ test/DonationButtonLandingPage_test.js
@@ -0,0 +1,12 @@
+var should = require('should'),
+    DonationButtonLandingPage = require('../lib/DonationButtonLandingPage.js')
+
+
+describe('DonationButtonLandingPage', function () {
+    before(function () {
+
+    })
+    it('should be awesome', function(){
+        DonationButtonLandingPage.awesome().should.eql('awesome')
+    })
+})
